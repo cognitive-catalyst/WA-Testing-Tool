a3427b53346d43bac5e94d3f21df3d55ecf58b41
Xinyun (Victor) Zhao <Victor.Zhao@ibm.com>
2018-04-12 11:11:25 -0500

    Add coroutine-based concurrent testing

--- CHANGELOG.md
+++ CHANGELOG.md
@@ -1,2 +1,6 @@
 # Changelog
 All notable changes to this project will be documented in this file.
+
+## 2018-04-12
+### Added
+- Coroutine-based concurrent testing is provided in sub-script.
--- testConversation.py
+++ testConversation.py
@@ -19,11 +19,12 @@
 """
 import os
 import csv
+import asyncio
 import pandas as pd
 from argparse import ArgumentParser
-from watson_developer_cloud import AssistantV1
+import aiohttp
 
-from utils import UTF_8, WCS_VERSION, CONFIDENCE_COLUMN, \
+from utils import UTF_8, CONFIDENCE_COLUMN, \
     UTTERANCE_COLUMN, PREDICTED_INTENT_COLUMN, \
     DETECTED_ENTITY_COLUMN, DIALOG_RESPONSE_COLUMN, \
     marshall_entity, save_dataframe_as_csv, INTENT_JUDGE_COLUMN, \
@@ -31,6 +32,41 @@ from utils import UTF_8, WCS_VERSION, CONFIDENCE_COLUMN, \
 
 test_out_header = [PREDICTED_INTENT_COLUMN, CONFIDENCE_COLUMN,
                    DETECTED_ENTITY_COLUMN, DIALOG_RESPONSE_COLUMN]
+
+
+async def post(session, json, url, sem):
+    """ Single post restrained by semaphore
+    """
+    async with sem:
+        async with session.post(url, json=json) as response:
+            return await response.json()
+
+
+async def fill_df(utterance, row_idx, out_df, workspace_id, wa_username,
+                  wa_password, sem):
+    """ Send utterance to Assistant and save response to dataframe
+    """
+    async with aiohttp.ClientSession(
+            auth=aiohttp.BasicAuth(wa_username, wa_password)) as session:
+        url = 'https://gateway.watsonplatform.net/assistant/api/v1/workspaces/{}/message?version=2018-02-16'.format(workspace_id)
+
+        resp = await post(session, {'input': {'text': utterance}}, url, sem)
+        intents = resp['intents']
+        if len(intents) != 0:
+            out_df.loc[row_idx, PREDICTED_INTENT_COLUMN] = \
+                intents[0]['intent']
+            out_df.loc[row_idx, CONFIDENCE_COLUMN] = \
+                intents[0]['confidence']
+
+        out_df.loc[row_idx, DETECTED_ENTITY_COLUMN] = \
+            marshall_entity(resp['entities'])
+
+        response_text = ''
+        response_text_list = resp['output']['text']
+        if len(response_text_list) != 0:
+            response_text = response_text_list.pop()
+
+        out_df.loc[row_idx, DIALOG_RESPONSE_COLUMN] = response_text
 
 
 def func(args):
@@ -65,29 +101,16 @@ def func(args):
     for column in test_out_header:
         out_df[column] = ''
 
-    conv = AssistantV1(username=args.username, password=args.password,
-                       version=WCS_VERSION)
-
-    for row_idx in range(out_df.shape[0]):
-        utterance = out_df.loc[row_idx, test_column]
-        resp = conv.message(workspace_id=args.workspace_id,
-                            input={'text': utterance},
-                            alternate_intents=True)
-        intents = resp['intents']
-        if len(intents) != 0:
-            out_df.loc[row_idx, PREDICTED_INTENT_COLUMN] = \
-                intents[0]['intent']
-            out_df.loc[row_idx, CONFIDENCE_COLUMN] = \
-                intents[0]['confidence']
-
-        out_df.loc[row_idx, DETECTED_ENTITY_COLUMN] = \
-            marshall_entity(resp['entities'])
-
-        response_text = ''
-        response_text_list = resp['output']['text']
-        if len(response_text_list) != 0:
-            response_text = response_text_list.pop()
-        out_df.loc[row_idx, DIALOG_RESPONSE_COLUMN] = response_text
+    # Applied coroutines
+    sem = asyncio.Semaphore(args.rate_limit)
+    loop = asyncio.get_event_loop()
+    tasks = [asyncio.ensure_future(fill_df(out_df.loc[row_idx, test_column],
+                                           row_idx, out_df, args.workspace_id,
+                                           args.username, args.password,
+                                           sem))
+             for row_idx in range(out_df.shape[0])]
+    loop.run_until_complete(asyncio.wait(tasks))
+    loop.close()
 
     if args.golden_intent_column is not None:
         golden_intent_column = args.golden_intent_column
@@ -105,7 +128,7 @@ def func(args):
 
 if __name__ == '__main__':
     PARSER = ArgumentParser(
-        description='Test assistant instance using utterance')
+        description='Test conversation instance using utterance')
     PARSER.add_argument('-i', '--infile', type=str, required=True,
                         help='File that contains test data')
     PARSER.add_argument('-o', '--outfile', type=str,
@@ -114,9 +137,9 @@ if __name__ == '__main__':
     PARSER.add_argument('-w', '--workspace_id', type=str, required=True,
                         help='Workspace ID')
     PARSER.add_argument('-u', '--username', type=str, required=True,
-                        help='Assistant service username')
+                        help='Conversation service username')
     PARSER.add_argument('-p', '--password', type=str, required=True,
-                        help='Assistant service password')
+                        help='Conversation service password')
     PARSER.add_argument('-t', '--test_column', type=str,
                         help='Test column name in input file')
     PARSER.add_argument('-m', '--merge_input', action='store_true',
@@ -124,5 +147,7 @@ if __name__ == '__main__':
                         help='Merge input content into test out')
     PARSER.add_argument('-g', '--golden_intent_column', type=str,
                         help='Golden column name in input file')
+    PARSER.add_argument('-r', '--rate_limit', type=int, default=1,
+                        help='Maximum number of requests per second')
     ARGS = PARSER.parse_args()
     func(ARGS)
