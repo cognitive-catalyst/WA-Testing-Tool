684e086cd4dd40a23d9e21f3b4a16eee658ee980
amblock <amblock@us.ibm.com>
2018-04-30 11:00:25 -0400

    added copyright and reduced line length to 77 chars

--- intent-description/get_intent_description.py
+++ intent-description/get_intent_description.py
@@ -1,3 +1,21 @@
+#! /usr/bin/python
+# coding: utf-8
+
+# Copyright 2018 IBM All Rights Reserved.
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#      http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+
+
 import argparse
 from watson_developer_cloud import ConversationV1
 import json
@@ -5,7 +23,11 @@ import csv
 
 
 def add_output_arg(parser):
-    parser.add_argument('--output', '-o', help='Output file', default='intent_desc.csv')
+    parser.add_argument(
+        '--output', '-o',
+        help='Output file',
+        default='intent_desc.csv'
+    )
 
 
 def get_remote_workspace(args):
@@ -37,20 +59,45 @@ def write_output(workspace_json, output_file):
 
 
 if __name__ == '__main__':
-    parser = argparse.ArgumentParser(description='Create Intent Descriptions file from workspace json', formatter_class=argparse.ArgumentDefaultsHelpFormatter)
+    parser = argparse.ArgumentParser(
+        description='Create Intent Descriptions file from workspace json',
+        formatter_class=argparse.ArgumentDefaultsHelpFormatter
+    )
 
     subparsers = parser.add_subparsers(help='help for subcommand')
 
-    credentials_parser = subparsers.add_parser('remote', help='Watson Assistant Credentials', formatter_class=argparse.ArgumentDefaultsHelpFormatter)
+    # Create parser for remote Watson Assistant instance
+    credentials_parser = subparsers.add_parser(
+        'remote',
+        help='Watson Assistant Credentials',
+        formatter_class=argparse.ArgumentDefaultsHelpFormatter
+    )
     add_output_arg(credentials_parser)
 
     requiredNamed = credentials_parser.add_argument_group('required arguments')
-    requiredNamed.add_argument('--user', '-u', help='Watson Assistant Username', required=True)
-    requiredNamed.add_argument('--password', '-p', help='Watson Assistant Password', required=True)
-    requiredNamed.add_argument('--workspace_id', '-w', help='Watson Assistant Workspace ID', required=True)
+    requiredNamed.add_argument(
+        '--user', '-u',
+        help='Watson Assistant Username',
+        required=True
+    )
+    requiredNamed.add_argument(
+        '--password', '-p',
+        help='Watson Assistant Password',
+        required=True
+    )
+    requiredNamed.add_argument(
+        '--workspace_id', '-w',
+        help='Watson Assistant Workspace ID',
+        required=True
+    )
     credentials_parser.set_defaults(func=get_remote_workspace)
 
-    local_parser = subparsers.add_parser('local', help='Local workspace file', formatter_class=argparse.ArgumentDefaultsHelpFormatter)
+    # Create parser for a local workspace json file
+    local_parser = subparsers.add_parser(
+        'local',
+        help='Local workspace file',
+        formatter_class=argparse.ArgumentDefaultsHelpFormatter
+    )
     add_output_arg(local_parser)
     local_parser.add_argument('json', help='Path to workspace json file')
     local_parser.set_defaults(func=get_local_workspace)
