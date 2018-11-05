942f2014ee1437c791eb0e47303c24ffa0f6fe89
Anuj Jain <jainanuj@us.ibm.com>
2018-11-05 17:03:11 -0500

    Update run.py
    Changes for IAM API key

--- run.py
+++ run.py
@@ -28,7 +28,7 @@ from utils import TRAIN_FILENAME, TEST_FILENAME, UTTERANCE_COLUMN, \
                   GOLDEN_INTENT_COLUMN, TEST_OUT_FILENAME, WORKSPACE_ID_TAG, \
                   WCS_VERSION, UTF_8, INTENT_JUDGE_COLUMN, BOOL_MAP, \
                   DEFAULT_TEST_RATE, POPULATION_WEIGHT_MODE, \
-                  DEFAULT_CONF_THRES, WCS_USERNAME_ITEM, WCS_PASSWORD_ITEM, \
+                  DEFAULT_CONF_THRES, WCS_USERNAME_ITEM, WCS_PASSWORD_ITEM, WCS_IAM_APIKEY_ITEM, \
                   WCS_CREDS_SECTION, CREATE_TEST_TRAIN_FOLDS_PATH, \
                   TRAIN_CONVERSATION_PATH, TEST_CONVERSATION_PATH, \
                   CREATE_PRECISION_CURVE_PATH, SPEC_FILENAME, \
@@ -70,14 +70,14 @@ def validate_config(fields, section):
                 format(field))
 
 
-def list_workspaces(username, password):
-    c = AssistantV1(username=username, password=password,
+def list_workspaces(username, password, iam_apikey):
+    c = AssistantV1(username=username, password=password, iam_apikey=iam_apikey,
                     version=WCS_VERSION, url=BASE_URL)
     return c.list_workspaces()
 
 
 def kfold(fold_num, temp_dir, intent_train_file, workspace_base_file,
-          figure_path, keep_workspace, username, password, weight_mode,
+          figure_path, keep_workspace, username, password, iam_apikey, weight_mode,
           conf_thres, partial_credit_table):
     FOLD_TRAIN = 'fold_train'
     FOLD_TEST = 'fold_test'
@@ -130,6 +130,7 @@ def kfold(fold_num, temp_dir, intent_train_file, workspace_base_file,
                       '-i', fold_param[FOLD_TRAIN],
                       '-n', fold_param[WORKSPACE_NAME],
                       '-u', username, '-p', password,
+                      '-a', iam_apikey,
                       '-w', workspace_base_file]
         train_processes_specs[
             subprocess.Popen(train_args, stdout=spec_file)] = spec_file
@@ -162,6 +163,7 @@ def kfold(fold_num, temp_dir, intent_train_file, workspace_base_file,
                          '-i', fold_param[FOLD_TEST],
                          '-o', fold_param[TEST_OUT],
                          '-u', username, '-p', password,
+                         '-a', iam_apikey,
                          '-t', UTTERANCE_COLUMN, '-g', GOLDEN_INTENT_COLUMN,
                          '-w', workspace_id, '-r', str(FOLD_TEST_RATE),
                          '-m']
@@ -219,12 +221,12 @@ def kfold(fold_num, temp_dir, intent_train_file, workspace_base_file,
                         workspace_id = json.load(f)[WORKSPACE_ID_TAG]
                         workspace_ids.append(workspace_id)
 
-            delete_workspaces(username, password, workspace_ids)
+            delete_workspaces(username, password, iam_apikey, workspace_ids)
 
 
 def blind(temp_dir, intent_train_file, workspace_base_file, figure_path,
           test_out_path, test_input_file, previous_blind_out, keep_workspace,
-          username, password, weight_mode, conf_thres, partial_credit_table, figure_title):
+          username, password, iam_apikey, weight_mode, conf_thres, partial_credit_table, figure_title):
     print('Begin {} with following details:'.format(BLIND_TEST.upper()))
     print('{}={}'.format(INTENT_FILE_ITEM, intent_train_file))
     print('{}={}'.format(WORKSPACE_BASE_ITEM, workspace_base_file))
@@ -264,6 +266,7 @@ def blind(temp_dir, intent_train_file, workspace_base_file, figure_path,
     train_args = [sys.executable, TRAIN_CONVERSATION_PATH,
                   '-i', intent_train_file, '-n', 'blind test',
                   '-u', username, '-p', password,
+                  '-a', iam_apikey,
                   '-w', workspace_base_file]
     with open(workspace_spec_json, 'w') as f:
         if subprocess.run(train_args, stdout=f).returncode == 0:
@@ -278,7 +281,7 @@ def blind(temp_dir, intent_train_file, workspace_base_file, figure_path,
         test_args = [sys.executable, TEST_CONVERSATION_PATH,
                      '-i', test_input_file,
                      '-o', test_out_path, '-m',
-                     '-u', username, '-p', password,
+                     '-u', username, '-p', password, '-a', iam_apikey,
                      '-t', UTTERANCE_COLUMN, '-g', GOLDEN_INTENT_COLUMN,
                      '-w', workspace_id,
                      '-r', str(MAX_TEST_RATE)]
@@ -299,11 +302,11 @@ def blind(temp_dir, intent_train_file, workspace_base_file, figure_path,
             raise RuntimeError('Failure in plotting curves')
     finally:
         if not keep_workspace:
-            delete_workspaces(username, password, [workspace_id])
+            delete_workspaces(username, password, iam_apikey, [workspace_id])
 
 
 def test(temp_dir, intent_train_file, workspace_base_file, test_out_path,
-         test_input_file, keep_workspace, username, password):
+         test_input_file, keep_workspace, username, password, iam_apikey):
     print('Begin {} with following details:'.format(STANDARD_TEST.upper()))
     print('{}={}'.format(INTENT_FILE_ITEM, intent_train_file))
     print('{}={}'.format(WORKSPACE_BASE_ITEM, workspace_base_file))
@@ -333,7 +336,7 @@ def test(temp_dir, intent_train_file, workspace_base_file, test_out_path,
     train_args = [sys.executable, TRAIN_CONVERSATION_PATH,
                   '-i', intent_train_file,
                   '-n', 'standard test',
-                  '-u', username, '-p', password,
+                  '-u', username, '-p', password, '-a', iam_apikey,
                   '-w', workspace_base_file]
     with open(workspace_spec_json, 'w') as f:
         if subprocess.run(train_args, stdout=f).returncode == 0:
@@ -348,7 +351,7 @@ def test(temp_dir, intent_train_file, workspace_base_file, test_out_path,
         if subprocess.run([sys.executable, TEST_CONVERSATION_PATH,
                            '-i', test_input_file,
                            '-o', test_out_path, '-m',
-                           '-u', username, '-p', password,
+                           '-u', username, '-p', password, '-a', iam_apikey,
                            '-w', workspace_id,
                            '-r', str(MAX_TEST_RATE)] + extra_params
                           ).returncode == 0:
@@ -357,7 +360,7 @@ def test(temp_dir, intent_train_file, workspace_base_file, test_out_path,
             raise RuntimeError('Failure in testing data')
     finally:
         if not keep_workspace:
-            delete_workspaces(username, password, [workspace_id])
+            delete_workspaces(username, password, iam_apikey, [workspace_id])
 
 
 def func(args):
@@ -372,15 +375,16 @@ def func(args):
             "Section '{}' is missing in config file".format(WCS_CREDS_SECTION))
 
     # Validate WCS creds
-    validate_config([WCS_USERNAME_ITEM, WCS_PASSWORD_ITEM],
+    validate_config([WCS_USERNAME_ITEM, WCS_PASSWORD_ITEM, WCS_IAM_APIKEY_ITEM],
                     config[WCS_CREDS_SECTION])
-
+    
     username = config[WCS_CREDS_SECTION][WCS_USERNAME_ITEM]
     password = config[WCS_CREDS_SECTION][WCS_PASSWORD_ITEM]
+    iam_apikey = config[WCS_CREDS_SECTION][WCS_IAM_APIKEY_ITEM]
 
     # List workspaces to see whether the creds is valid.
     # SDK has no method for validation purpose
-    list_workspaces(username, password)
+    list_workspaces(username, password, iam_apikey)
 
     print('Credentials are correct')
 
@@ -396,7 +400,7 @@ def func(args):
     if subprocess.run([sys.executable, WORKSPACE_PARSER_PATH,
                        '-i', default_section[WORKSPACE_ID_ITEM],
                        '-o', temp_dir,
-                       '-u', username, '-p', password],
+                       '-u', username, '-p', password, '-a', iam_apikey],
                       stdout=subprocess.PIPE).returncode == 0:
         print('Parsed workspace')
     else:
@@ -448,7 +452,7 @@ def func(args):
               workspace_base_file=workspace_base_file,
               figure_path=default_section[FIGURE_PATH_ITEM],
               keep_workspace=keep_workspace,
-              username=username, password=password,
+              username=username, password=password, iam_apikey=iam_apikey,
               weight_mode=weight_mode, conf_thres=conf_thres_str,
               partial_credit_table=partial_credit_table)
     else:
@@ -465,7 +469,7 @@ def func(args):
                   test_out_path=default_section[TEST_OUT_PATH_ITEM],
                   previous_blind_out=previous_blind_out,
                   keep_workspace=keep_workspace,
-                  username=username, password=password,
+                  username=username, password=password, iam_apikey=iam_apikey,
                   weight_mode=weight_mode, conf_thres=conf_thres_str,
                   partial_credit_table=partial_credit_table,
                   figure_title=default_section[BLIND_FIGURE_TITLE])
@@ -477,7 +481,8 @@ def func(args):
                  test_out_path=default_section[TEST_OUT_PATH_ITEM],
                  keep_workspace=keep_workspace,
                  username=username,
-                 password=password)
+                 password=password,
+                 iam_apikey=iam_apikey)
         else:
             raise ValueError("Unknown mode '{}'".format(mode))
 
