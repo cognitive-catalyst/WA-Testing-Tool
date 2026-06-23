af1c0f9cf39940e087b2365cdb34d50230398de4
Eric Keilty <eric.keilty@ibm.com>
2026-06-23 13:23:03 -0400

    fix bug with variable metadata output format

--- skill_analytics/cli/variable_metadata.py
+++ skill_analytics/cli/variable_metadata.py
@@ -3,6 +3,7 @@ from argparse import ArgumentParser, RawDescriptionHelpFormatter
 from config.config import get_config
 from src.analyzers import VariableAnalyzer
 from src.models.assistant import Assistant
+from src.output_handlers import OutputFormat
 
 from .utils.file_path_helper import (
     create_directory,
@@ -77,6 +78,7 @@ def main():
             include_step_variables=args.step_variables,
             include_result_variables=args.result_variables,
             include_system_variables=args.system_variables,
+            return_as=OutputFormat.DATAFRAME,
         )
 
     variable_metadata_df = variable_metadata_df.sort_values("id")
